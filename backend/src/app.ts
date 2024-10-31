2671f2cc83898977b9daaf2770ddd1f5b0a0146d
Nikolai Musatov <musatovnikolajj@rambler.ru>
2024-10-31 09:52:06 +0400

    add rateLimiter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,10 +13,12 @@ import rateLimit from 'express-rate-limit'
 
 const { PORT = 3000 } = process.env
 const app = express()
+
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: 'Too many requests from this IP, please try again later.',
+    limit: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
 })
 
 app.use(cookieParser())
